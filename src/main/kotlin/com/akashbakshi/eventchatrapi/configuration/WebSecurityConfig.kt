626bf3032e8ebf50b63d7ca3c7eeaaa17b904c22
akashbakshi <akashbakshi@bubble-tech.com>
2019-12-08 03:20:46 -0500

    - Added cors fix

--- src/main/kotlin/com/akashbakshi/eventchatrapi/configuration/WebSecurityConfig.kt
+++ src/main/kotlin/com/akashbakshi/eventchatrapi/configuration/WebSecurityConfig.kt
@@ -1,12 +1,17 @@
 package com.akashbakshi.eventchatrapi.configuration
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
 
 @Configuration
 @EnableWebSecurity()
@@ -14,12 +19,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 class WebSecurityConfig: WebSecurityConfigurerAdapter (){
 
     override fun configure(http: HttpSecurity) {
-        http.csrf().and().cors().disable().authorizeRequests().antMatchers("/api/**").hasAnyRole("ADMIN").and().httpBasic();
+        http.cors().and().authorizeRequests().antMatchers("/api/**").hasAnyRole("ADMIN").and().httpBasic();
+        http.csrf().disable()
+        http.headers().frameOptions().disable()
     }
 
     @Autowired
     @Throws(Exception::class)
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
         auth.inMemoryAuthentication().withUser("eventadmin").password("{noop}3v3ntPass").roles("ADMIN")
+    }
+}
+
+
+@Configuration
+class CorsConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:5000")
+            }
+        }
     }
 }
