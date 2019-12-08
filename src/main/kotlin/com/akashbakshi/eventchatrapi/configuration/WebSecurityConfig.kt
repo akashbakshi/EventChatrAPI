19261af2a3a0f4bd76b347e7e04518447dbc269a
akashbakshi <akashbakshi@bubble-tech.com>
2019-12-08 03:29:35 -0500

    - Added cors fix

--- src/main/kotlin/com/akashbakshi/eventchatrapi/configuration/WebSecurityConfig.kt
+++ src/main/kotlin/com/akashbakshi/eventchatrapi/configuration/WebSecurityConfig.kt
@@ -1,7 +1,6 @@
 package com.akashbakshi.eventchatrapi.configuration
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -9,8 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
 @Configuration
@@ -23,7 +22,6 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter (){
         http.csrf().disable()
         http.headers().frameOptions().disable()
     }
-
     @Autowired
     @Throws(Exception::class)
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
@@ -33,15 +31,9 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter (){
 
 
 @Configuration
-class CorsConfiguration {
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurerAdapter() {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                        .allowedMethods("*")
-                        .allowedOrigins("http://localhost:5000")
-            }
-        }
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
     }
 }
